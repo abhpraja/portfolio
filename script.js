3426caf582bf4cfdd6e1b8b1930683d73b386272
Abhishek Prajapati <146121974+abhpraja@users.noreply.github.com>
2025-06-10 02:27:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,78 @@ function openSidebar() {
 function closeSidebar(){
   navbar.classList.remove('show');
 }
+
+gsap.from("#about .about-details",{
+  y: 100,
+  opacity: 0,
+  delay: 1,
+  duration: 2,
+  stagger:1,
+})
+
+gsap.from(".proj-info", {
+  y: 100,
+  backgroundColor: "Black",
+  color: "white",
+  opacity: 0,
+  duration: 2,
+  stagger:0.5,
+  scrollTrigger: {
+    trigger: ".proj-info",
+    scroller: "body",
+    // markers: true,
+    start: "top 60%",
+    end: "bottom 60%",
+  }
+})
+
+// const openBtn = document.getElementById("openModal");
+// const closeBtn = document.getElementById("closeModal");
+// const modal = document.getElementById("modal");
+
+// openBtn.addEventListener("click", () => {
+//   modal.classList.add("open");
+// });
+
+// closeBtn.addEventListener("click", () => {
+//   modal.classList.remove("open");
+// })
+
+
+const openBtns = document.querySelectorAll("#openModal");
+const closeBtns = document.querySelectorAll("#closeModal");
+const modals = document.querySelectorAll(".modal");
+
+openBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    modals[index].classList.add("open");
+  });
+});
+
+closeBtns.forEach((btn, index) => {
+  btn.addEventListener("click", () => {
+    modals[index].classList.remove("open");
+  });
+});
+
+function closemodalBtn(modalElement) {
+  modalElement.classList.remove("open");
+}
+
+
+//  const paths = document.querySelectorAll("#abhiLogo path");
+
+//   paths.forEach(path => {
+//     const length = path.getTotalLength();
+//     path.style.strokeDasharray = length;
+//     path.style.strokeDashoffset = length;
+//   });
+
+//   gsap.to("#abhiLogo path", {
+//     strokeDashoffset: 0,
+//     duration: 50,
+//     stagger: 0.2,
+//     ease: "power2.out"
+//   });
+
+
