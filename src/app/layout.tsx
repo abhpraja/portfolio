226408ac58fa64436a6124b95645da968b833294
Abhishek Prajapati <abhpraja235@gmail.com>
2025-07-03 19:13:56 +0530

    Add Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-
+import { Analytics } from '@vercel/analytics/react';
 import "./globals.css";
 import { twMerge } from "tailwind-merge";
 
@@ -25,6 +25,7 @@ export default function RootLayout({
         )}
       >
         {children}
+         <Analytics />
       </body>
     </html>
   );
